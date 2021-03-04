7299ba4840538d7d397321c955d5ebfe4d71b9e7
past0073 <past0073@umn.edu>
2021-03-03 19:20:44 -0600

    Added htmlRoutes.js and two required routes.

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}));
-app.use(espress.json());
+app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static('/Develop/public'));
 
-require('./develop/routes/apiRoutes')(app);
-require('./develop/routes/htmlRoutes')(app);
+require('./Develop/Routes/apiRoutes')(app);
+require('./Develop/Routes/htmlRoutes')(app);
 
 app.listen(PORT, function() {
     console.log(`Listening on PORT: ${PORT}`)
