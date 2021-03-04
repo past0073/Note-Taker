8c9538ec4cfee729fa943d4eefe3e6d72a0ca147
past0073 <past0073@umn.edu>
2021-03-03 21:15:10 -0600

    Fixed pathing error in htmlRoutes.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-app.use(express.static('/Develop/public'));
+app.use(express.static('public'));
 
 require('./Develop/Routes/apiRoutes')(app);
 require('./Develop/Routes/htmlRoutes')(app);
